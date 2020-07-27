1e75f459b2e23534e339229aa421f365c0e5fa4f
BoredFish <BoredFish@users.noreply.github.com>
2020-07-27 12:36:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-const Discord = require('discordjs');
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token } = require('./config.json');
+
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.name, command);
+}
 
 client.once('ready', () => {
     console.log("Ready!");
+});
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const commandName = args.shift().toLowerCase();
+
+    if (!client.commands.has(commandName)) return;
+    
+    const command = client.commands.get(commandName);
+
+    try {
+	    command.execute(message, args);
+    } 
+    catch (error) {
+	    console.error(error);
+	    message.reply('there was an error trying to execute that command!');
+    }
+
+});
 
 
-})
+client.login('haha you arent getting this');
