9c92df2feb973c34cdb9c7c0856a781f4f485b3b
BoredFish <BoredFish@users.noreply.github.com>
2020-07-27 13:03:28 -0400

    i fix yes

--- commands/ping.js
+++ commands/ping.js
@@ -7,7 +7,7 @@ module.exports = {
         var ping = Date.now() -message.createdTimestamp + " ms";
         let embed = new MessageEmbed()
         .setColor('#ff9100')
-        .addField('Pong!', '`' + ping, true)
+        .addField('Pong!', ping, true)
         message.channel.send(embed);
     }
 
