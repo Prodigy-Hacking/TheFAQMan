50433783a61181063f80867a5edda9c147846994
BoredFish <BoredFish@users.noreply.github.com>
2020-07-28 11:35:34 -0400

    Update battleHacks.js

--- commands/battleHacks.js
+++ commands/battleHacks.js
@@ -1,29 +1,26 @@
 const { description } = require("./ping");
-
+const { MessageEmbed } = require('discord.js');
 module.exports = {
-    name: 'Battle',
+    name: 'battle',
     description: 'Battle hacks',
     execute(message, args){
-        let embed = new MessageEmbed
-        .setHeader('Battle Hacks')
+        let embed = new MessageEmbed()
         .setColor('#ff9100')
-        .addFeilds(
-            { name: "", value: '[battleLosses.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/battleLoses.js)'},
-            { name: "", value: '[bossDifficulty.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/bossDifficulty.js)'},
-            { name: "", value: '[escapeBattle.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/escapeBattle.js)'},
-            { name: "", value: '[freeCatches.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/freeCatches.js)'},
-            { name: "", value: '[healTeam.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/healTeam.js)'},
-            { name: "", value: '[immortal.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/immortal.js)'},
-            { name: "", value: '[infiniteCatchAttempts.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/infiniteCatchAttempts.js)'},
-            { name: "", value: '[instantWin.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/instantWin.js)'},
-            { name: "", value: '[invincibility.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/invincibility.js)'},
-            { name: "", value: '[monstersLevel1.js ](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/monstersLevel1.js)'},
-            { name: "", value: '[oneShotEnemy.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/oneShotEnemy.js)'},
-            { name: "", value: '[skipEnemyTurn.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/skipEnemyTurn.js)'},
-
-
+        .addFields(
+            { name: "Battle Losses", value: '[battleLosses.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/battleLoses.js)'},
+            { name: "Boss Difficulty", value: '[bossDifficulty.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/bossDifficulty.js)'},
+            { name: "Escape Battle", value: '[escapeBattle.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/escapeBattle.js)'},
+            { name: "Free Catches!", value: '[freeCatches.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/freeCatches.js)'},
+            { name: "Heal Team", value: '[healTeam.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/healTeam.js)'},
+            { name: "Immortal", value: '[immortal.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/immortal.js)'},
+            { name: "Infinite Catch Attempts", value: '[infiniteCatchAttempts.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/infiniteCatchAttempts.js)'},
+            { name: "Instant Win!", value: '[instantWin.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/instantWin.js)'},
+            { name: "Invincibility", value: '[invincibility.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/invincibility.js)'},
+            { name: "All Monsters Level 1", value: '[monstersLevel1.js ](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/monstersLevel1.js)'},
+            { name: "One Shot Enemy", value: '[oneShotEnemy.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/oneShotEnemy.js)'},
+            { name: "Skip Enemy Turn", value: '[skipEnemyTurn.js](https://github.com/Prodigy-Hacking/ProdigyMathGameHacking/blob/master/hacks/Battle/skipEnemyTurn.js)'}
         )
-
+        message.channel.send(embed);
     }
 
-}
+};
