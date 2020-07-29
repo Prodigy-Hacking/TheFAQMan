86e5a30730123f67c1cce59a87df416e44421f75
DaChickenKing <dck.dachickenking@gmail.com>
2020-07-28 20:15:05 -0700

    bro this isnt spaghetti dinner

--- commands/ping.js
+++ commands/ping.js
@@ -3,12 +3,11 @@ const { MessageEmbed } = require("discord.js")
 module.exports = {
     name: 'ping',
     description: 'Ping tester',
-    execute(message, args){
+    execute(message, args) {
         var ping = Date.now() -message.createdTimestamp + " ms";
         let embed = new MessageEmbed()
         .setColor('#ff9100')
         .addField('Pong!', ping, true)
         message.channel.send(embed);
     }
-
 };
