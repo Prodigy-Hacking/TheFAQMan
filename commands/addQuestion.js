b546f8f8b96c5d14bdf08c47e5536cc2ff26c468
BoredFish <BoredFish@users.noreply.github.com>
2020-08-04 12:14:25 -0400

    yes work excite

--- commands/addQuestion.js
+++ commands/addQuestion.js
@@ -3,7 +3,6 @@ const Enmap = require("enmap");
 const jsonfile = require("jsonfile");
 const file = "./questions.json";
 const { MessageEmbed } = require("discord.js");
-const BugReport = require("../utils/bugReport.js");
 function between(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
@@ -14,36 +13,11 @@ module.exports = {
   name: "addfaq",
   description: "Creates a FAQ Q&A",
   execute(message, args) {
-    const addfaqdiag = {
-        question: "Please enter your question",
-        answer: "Please enter the answer"
-    }
-    let embed = new MessageEmbed()
+    
+    let embedcollecttrigger = new MessageEmbed()
         .setColor("#ff9100")
-        .addFields(
-            {name: "This is the command to add a quetion and answer!", value: "Otherwise it won't work!"},
-            {name: "You have to do it very specifically. It has to be formatted like this", value: "Question:[question here] Answer: [answer here]. Now send"}
-        )
-    message.channel.send(embed);
-    async function FAQAandA(channel) {
-        let dialogueResults = [];
-        for (const line of Object.entries(bugFormDialogue)) {
-            const embedcollecttrigger = new MessageEmbed()
-            .setColor("#ff9100")
-            .addField("Now, type your FAQ!", "please")
-            await channel.send(FAQQandA).then(async () => await waitForResponse(dialogueResults));
-        }
-    }
-    async function waitForResponse(dialogueResults) {
-        let filter = m => m.author.id === member.id && !m.author.bot;
-        await channel.awaitMessages((() => true), {max: 1}).then(async (collected) => {
-            collected.forEach(async (msg) => {
-                await Promise.resolve(
-                    filter(msg) ? msg.content : await waitForResponse(dialogueResults)
-                ).then(val => dialogueResults.push(val.slice(0, 800)));
-            });
-        });
-    }
+        .addField("Enter your question, then send that", "Then after that, send your answer. It will be 2 messages")
+    message.channel.send(embedcollecttrigger)
     message.channel
       .awaitMessages(m => m.author.id == message.author.id, {
         max: 2,
@@ -52,7 +26,7 @@ module.exports = {
       .then(collected => {
         var ID;
         ID = Math.random().toString(36).substr(2, 9);
-        const obj = { ID, FAQ: collected.first().content};
+        const obj = { ID, Question: collected.first().content, Answer: collected.last().content};
         jsonfile.writeFileSync(file, obj , { spaces: 2, flag: "a", EOL: '\r\n' });
         message.channel.send("FAQ Logged");
         // The collected.first().content is the first thing the sender of the initial message chats
